92bd9142ec41d75f42efe16f44bbcfa3eea115ef
Mahlia <48108230+MahliaCode@users.noreply.github.com>
2019-05-05 12:30:26 +0200

    rest

--- Risiko/src/risiko/local/domain/exceptions/AnzahlEinheitenFalschException.java
+++ Risiko/src/risiko/local/domain/exceptions/AnzahlEinheitenFalschException.java
@@ -2,8 +2,8 @@ package risiko.local.domain.exceptions;
 
 public class AnzahlEinheitenFalschException extends Exception{
 
-	public AnzahlEinheitenFalschException(int verschiebbareEinheiten) {
-		super("Du kannst maximal " + verschiebbareEinheiten + " Einheiten aus dieser Provinz verschieben.");
+	public AnzahlEinheitenFalschException(int einheiten) {
+		super("Du musst mehr als 0 und maximal " + einheiten + " Einheiten nutzen.");
 	}
 	public AnzahlEinheitenFalschException(String message) {
 		super(message);
