9ebd2a316d6fda60f5bf9b40951d9480610f5f0d
NiJoW <48721890+NiJoW@users.noreply.github.com>
2019-06-03 22:36:12 +0200

    merged

--- Risiko/src/risiko/local/ui/gui/swing/game/KartenPanel.java
+++ Risiko/src/risiko/local/ui/gui/swing/game/KartenPanel.java
@@ -19,6 +19,7 @@ import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -36,22 +37,104 @@ public class KartenPanel extends JPanel {
 	int width = 1024;
 	int height = 525;
 	
-	public KartenPanel(Risiko risiko, InformationsPanel informationsPanel, int aktuellerSpielerID, int screenWidth, int screenHeight) {
+	public KartenPanel(Risiko risiko, InformationsPanel informationsPanel, int aktuellerSpielerID, int screenWidth, int screenHeight,  JLayeredPane lp) {
 		this.risiko = risiko;
 		this.informationsPanel = informationsPanel;
 		
 		this.aktuellerSpielerID = aktuellerSpielerID;
 		System.out.println(aktuellerSpielerID);
 		String spielername  = risiko.getSpielerName(aktuellerSpielerID);
-		initialize(spielername, screenWidth, screenHeight);
+		initialize(spielername, screenWidth, screenHeight, lp);
 	}
 	
-	private void initialize(String spielername, int screenWidth, int screenHeight) {
+	private void initialize(String spielername, int screenWidth, int screenHeight, JLayeredPane orginaleLayerPane) {
 		this.setSize(width,height);
 		this.setPreferredSize(new Dimension(width,height));
 		
+		JLayeredPane orginalLayerPane = orginaleLayerPane;
+		JLayeredPane kartenLayer = new JLayeredPane();
+		kartenLayer.setBounds(0, 0, 1024, 525);
+		kartenLayer.setLayout(null);
+		
 		ImagePanel myImage = new ImagePanel(this);
-		this.add(myImage);
+//		this.add(myImage);
+		
+		ImagePanelZeigen myImageZeigen = new ImagePanelZeigen(this);
+
+//		ImageIcon face = new ImageIcon("images/Gewonnen.png"); 
+//		JLabel alaska = new JLabel(face); //new JLabel(face);
+//		alaska.setBounds(85, 65, 10, 10);
+//		alaska.setPreferredSize(new Dimension(100,100));
+//		alaskaText.setPreferredSize(new Dimension(100,100));
+		JLabel alaskaText = new JLabel("3"); //new JLabel(face);
+		alaskaText.setLocation(86, 62);
+		alaskaText.setSize(new Dimension(10,10));
+		alaskaText.setBackground(Color.WHITE);
+		alaskaText.setOpaque(true);
+		kartenLayer.add(alaskaText);
+
+		JLabel nwTerriteriumText  = new JLabel("3");
+		nwTerriteriumText.setLocation(172, 62);
+		nwTerriteriumText.setSize(new Dimension(10,10));
+		nwTerriteriumText.setBackground(Color.WHITE);
+		nwTerriteriumText.setOpaque(true);
+		kartenLayer.add(nwTerriteriumText);
+
+		JLabel ontarioText  = new JLabel("4");
+		ontarioText.setLocation(150, 100);
+		ontarioText.setSize(new Dimension(10,10));
+		ontarioText.setBackground(Color.WHITE);
+		ontarioText.setForeground(Color.RED);
+		ontarioText.setOpaque(true);
+		kartenLayer.add(ontarioText);
+
+		JLabel argentinienText  = new JLabel("6");
+		argentinienText.setLocation(270, 478);
+		argentinienText.setSize(new Dimension(10,10));
+		argentinienText.setBackground(Color.WHITE);
+		argentinienText.setOpaque(true);
+		kartenLayer.add(argentinienText);
+
+		JLabel islandText  = new JLabel("8");
+		islandText.setLocation(412, 62);
+		islandText.setSize(new Dimension(10,10));
+		islandText.setBackground(Color.WHITE);
+		islandText.setForeground(Color.RED);
+		islandText.setOpaque(true);
+		kartenLayer.add(islandText);
+
+		JLabel groﬂbritanienText  = new JLabel("3");
+		groﬂbritanienText.setLocation(350, 62);
+		groﬂbritanienText.setSize(new Dimension(10,10));
+		groﬂbritanienText.setBackground(Color.WHITE);
+		groﬂbritanienText.setOpaque(true);	
+
+		JLabel kamtschatkaText  = new JLabel("9");
+		kamtschatkaText.setLocation(918, 62);
+		kamtschatkaText.setSize(new Dimension(10,10));
+		kamtschatkaText.setBackground(Color.WHITE);
+		kamtschatkaText.setForeground(Color.RED);
+		kamtschatkaText.setOpaque(true);
+		kartenLayer.add(kamtschatkaText);
+
+		JLabel ostAustralienText  = new JLabel("7");
+		ostAustralienText.setLocation(920, 430);
+		ostAustralienText.setSize(new Dimension(10,10));
+		ostAustralienText.setBackground(Color.WHITE);
+		ostAustralienText.setOpaque(true);
+		kartenLayer.add(ostAustralienText);
+
+
+
+
+
+	//	kartenLayer.add(alaska);
+		kartenLayer.add(myImageZeigen);
+		kartenLayer.add(myImage);
+
+		orginalLayerPane.add(kartenLayer);
+		
+		
 		this.setVisible(true);
 
 	}
